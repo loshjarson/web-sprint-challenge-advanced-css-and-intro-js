ee78116385f82ee4781bf2c41b12900a9f91d603
Joshua Larson <74164891+loshjarson@users.noreply.github.com>
2021-05-01 20:51:01 -0400

    completed task 6

--- index.js
+++ index.js
@@ -291,8 +291,9 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, object) {
+  array.push(object);
+  return array;
 }
 
 
